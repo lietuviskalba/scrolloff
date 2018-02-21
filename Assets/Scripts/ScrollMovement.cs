8ccd75271168d48d104bcf42434b1469d27395a2
LietuvisKalba <mantasling@gmail.com>
2018-02-21 11:09:58 -0500

    first commit

---
+++ Assets/Scripts/ScrollMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollMovement : MonoBehaviour {
+
+    private GameObject player;
+
+    public void MoveHorizontal(float newValue)
+    {
+        Vector3 pos = player.transform.position;
+        pos.x = newValue;
+        player.transform.position = pos;
+    }
+    
+    public void MoveVertical(float newValue)
+    {
+        Vector3 pos = player.transform.position;
+        pos.y = newValue;
+        player.transform.position = pos;
+    }
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+}
